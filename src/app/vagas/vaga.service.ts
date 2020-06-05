cbbfe5120a1affb6a90c2d5b07d0e8fe33ed2bde
Anfrisio Neto <abnt.neto@gmail.com>
2020-06-05 09:00:47 -0300

    VAGA - add metodo list e persist

--- src/app/vagas/vaga.service.ts
+++ src/app/vagas/vaga.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Vaga } from './vaga.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VagaService {
-
+  
   constructor(private http: HttpClient) {
   }
-
+  
   getVagas() {
-    return this.http.get<any>('http://localhost:3000/vagas');
+    return this.http.get<Vaga[]>('http://localhost:3000/vagas');
+  }
+
+  persist(vaga: Vaga) {
+    return this.http.post<Vaga>('http://localhost:3000/vagas', vaga);
   }
 }
--- src/app/vagas/vagas/vagas.component.ts
+++ src/app/vagas/vagas/vagas.component.ts
@@ -10,6 +10,9 @@ import { Vaga } from '../vaga.model';
 export class VagasComponent implements OnInit {
 
   vagas: Vaga[] = [];
+  vaga = new Vaga();
+
+   isModalActive = false;
 
   constructor(private service: VagaService) { }
 
@@ -21,6 +24,25 @@ export class VagasComponent implements OnInit {
     this.service.getVagas()
         .subscribe(resp => {
           this.vagas = resp;
+        });
+  }
+
+  toggleModal() {
+
+    this.isModalActive = !this.isModalActive;
+    this.vaga = new Vaga();
+
+    
+
+  }
+
+  salvar() {
+    this.service.persist(this.vaga)
+        .subscribe(resp => {
+          //TODO: Criar módulo de mensagens
+          console.log(resp);
+          this.toggleModal();
+          this.getVagas();
         });
   }
 
